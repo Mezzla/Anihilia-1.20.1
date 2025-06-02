ad4ff84cb7cc763949c7cd55aa155988de437642
Melker Johansson <smrjn003@edu.karlstad.se>
2025-06-02 23:59:26 +0200

    Initial commit

---
+++ src/main/java/me/mezzla/AnihiliaClient.java
@@ -0,0 +1,11 @@
+package me.mezzla;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnihiliaClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
